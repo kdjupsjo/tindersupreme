f6a333dad153e5d6745e4fd43147dcc25c24dae2
Kristoffer Djupsjö <kristoffer.djupsjo@xlent.se>
2018-02-06 14:44:07 +0100

    Add BLL and Account logic

---
+++ TinderSupreme/TinderSupreme/BLL/AccountBLL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace TinderSupreme.BLL
+{
+    public class AccountBLL
+    {
+        public static String NewUser(String userName, String password, String verifiedPassWord)
+        //
+        {
+            //    String s = "";
+            //    //Check if username already exists
+            //    if !IsUsernameInUse(userName))
+            //    {
+            //        //Username don't exists, check if the password verfication is correct
+            //        if (password == verifiedPassWord)
+            //        {
+            //            //Store username in database
+
+            //            s += "Username succesfully created and stored in database"; 
+
+            //        }
+            //        else
+            //        {
+            //            s += "Password verification failed"; 
+            //        }
+
+            //    }
+            //    else
+            //    {
+            //        s += "Username already exists"; 
+            //    }
+            //    return s;
+
+            //
+            return "";
+        }
+        
+    }
+}
