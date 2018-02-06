9e357259730cbc935eb01e82b6e38e932321712c
Johannes Eriksson Wallin <johannes.eriksson-wallin@xlent.se>
2018-02-06 11:08:18 +0100

    Code first init

---
+++ TinderSupreme/TinderSupreme/DatabaseContext/TSContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TinderSupreme
+{
+    public class Account
+    {
+        [Key]
+        [Column(TypeName = "VARCHAR")]
+        [StringLength(256)]
+        public String UserName { get; set; }
+        public String Password { get; set; }
+    }
+
+    public class TSContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
---
+++ TinderSupreme/TinderSupreme/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace TinderSupreme.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TinderSupreme.TSContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(TinderSupreme.TSContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
