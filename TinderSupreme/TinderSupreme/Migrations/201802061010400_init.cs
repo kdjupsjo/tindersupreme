17f0d175383e8c753205d7b3e9f99ee779fd3281
Kristoffer Djupsjö <kristoffer.djupsjo@xlent.se>
2018-02-06 11:12:57 +0100

    Add migration, db

---
+++ TinderSupreme/TinderSupreme/Migrations/201802061010400_init.cs
@@ -0,0 +1,26 @@
+namespace TinderSupreme.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Accounts",
+                c => new
+                    {
+                        UserName = c.String(nullable: false, maxLength: 256, unicode: false),
+                        Password = c.String(),
+                    })
+                .PrimaryKey(t => t.UserName);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Accounts");
+        }
+    }
+}
